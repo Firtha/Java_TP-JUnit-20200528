3c42babdeb508d3ed05e1c8c35d01d56c15e53c2
Svensson Jeremy <svensson.jeremy94@gmail.com>
2020-05-28 10:47:59 +0200

    First commit with class SommeArgent

---
+++ src/junit/monprojet/test/SommeArgentTest.java
@@ -0,0 +1,14 @@
+package junit.monprojet.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SommeArgentTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
