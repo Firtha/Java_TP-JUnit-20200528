ae19372be232bf721f1afd6b62ef13f9e1add718
Svensson Jeremy <svensson.jeremy94@gmail.com>
2020-05-28 11:19:12 +0200

    Test file but problems with junit4 use

--- src/junit/monprojet/test/SommeArgentTest.java
+++ src/junit/monprojet/test/SommeArgentTest.java
@@ -1,13 +1,22 @@
 package junit.monprojet.test;
 
 import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import junit.monprojet.SommeArgent;
 
 public class SommeArgentTest {
 
 	@Test
 	public void test() {
+		SommeArgent testObject = new SommeArgent(420, "GreenTokens");
+		SommeArgent equalObject = new SommeArgent(420, "GreenTokens");
+		SommeArgent diffObject = new SommeArgent(024, "TokensGreen");
+		
+		if Assert.assertTrue(testObject.equals(equalObject)){
+			System.out.println("Test and equal object are equals");
+		}
 		fail("Not yet implemented");
 	}
 
